86bbbc337898ed04279eaad1c383b4d44add05ed
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-04-24 13:04:27 -0300

    adding pipe docs

---
+++ functions/hello/config.py
@@ -0,0 +1,15 @@
+from infra.services import Services
+
+class HelloConfig:
+    def __init__(self, services: Services) -> None:
+
+        function = services.aws_lambda.create_function(
+            name="Hello",
+            path="./functions/hello",
+            description="A private function",
+            
+        )
+
+        services.api_gateway.create_endpoint("GET", "/hello", function, public=True)
+
+            
